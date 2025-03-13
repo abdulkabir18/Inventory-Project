9e19408c1a067daab2e0cb3c36ad2f3f88575bae
Abdulkabir fagbohun <122563248+abdulkabir18@users.noreply.github.com>
2025-03-13 11:45:43 +0100

    second commit

--- obj/Debug/net8.0/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventoryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a6b8979dfeff72c45bc4a5317de0b7382af9d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
